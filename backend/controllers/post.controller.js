43160eeda13d5adea7706e023cf373bb65f18abd
Rofilio <rofiliofernandes6@gmail.com>
2025-12-14 23:05:00 +0530

    Update post.controller.js

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,3 @@
-import sharp from "sharp";
 import cloudinary from "../utils/cloudinary.js";
 import { Post } from "../models/post.model.js";
 import { User } from "../models/user.model.js";
@@ -262,3 +261,4 @@ export const bookmarkPost = async (req,res) => {
     }
 
 }
+
