af3e9dde965f916118f1ff6ae4e1d50a839b012a
Rofilio <rofiliofernandes6@gmail.com>
2025-12-16 21:01:08 +0530

    Enhance post schema with text and type fields
    Added 'text' and 'type' fields to the post schema.

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -10,4 +10,9 @@ const postSchema = new mongoose.Schema({
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }]
 }, { timestamps: true });
 
+text: { type: String, maxlength: 280 }, // tweet
+type: { type: String, enum: ["image", "text"], required: true },
+
+
 export const Post = mongoose.model("Post", postSchema);
+
