bd3cf8f08324a604cc7ab40febfc30339ca811ef
Rofilio <rofiliofernandes6@gmail.com>
2025-12-20 21:06:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,9 @@ import { initSocket } from "./socket/socket.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const server = http.createServer(app);
 
-app.use("/api/v1/auth", authRoutes);
 
 app.use(passport.initialize());
 
@@ -29,11 +29,12 @@ app.use(passport.initialize());
 
 
 // Middlewares
-app.use(express.json());
 app.use(cors({
     origin: process.env.CLIENT_URL || "*",
     credentials: true
 }));
+
+app.use("/api/v1/auth", authRoutes);
 
 // Connect DB
 connectDB();
@@ -53,6 +54,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
