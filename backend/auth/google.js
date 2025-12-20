2544122e5bf6d292ab6fccc589df7c847d25633d
Rofilio <rofiliofernandes6@gmail.com>
2025-12-20 14:37:34 +0530

    Update google.js

--- backend/auth/google.js
+++ backend/auth/google.js
@@ -1,5 +1,6 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 passport.use(
@@ -7,21 +8,27 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.SERVER_URL}/auth/google/callback`,
+      callbackURL: "/api/v1/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
-      let user = await User.findOne({ email: profile.emails[0].value });
+      try {
+        const email = profile.emails[0].value;
 
-      if (!user) {
-        user = await User.create({
-          username: profile.displayName,
-          email: profile.emails[0].value,
-          password: "google-oauth",
-          profilePicture: profile.photos[0].value,
-        });
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            username: profile.displayName.replace(/\s+/g, "").toLowerCase(),
+            email,
+            password: "google-auth",
+            profilePicture: profile.photos[0]?.value,
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
       }
-
-      return done(null, user);
     }
   )
 );
