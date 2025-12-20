82a2dda41f4bbda03957e23bdb51cc34c27e39eb
Rofilio <rofiliofernandes6@gmail.com>
2025-12-20 20:06:25 +0530

    Create passport.js

---
+++ backend/utils/passport.js
@@ -0,0 +1,40 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.BACKEND_URL}/api/v1/auth/google/callback`
+    },
+    async (_, __, profile, done) => {
+      try {
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        if (!user) {
+          user = await User.create({
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            profilePicture: profile.photos[0].value,
+            password: "google-oauth"
+          });
+        }
+
+        const token = jwt.sign(
+          { id: user._id },
+          process.env.JWT_SECRET,
+          { expiresIn: "7d" }
+        );
+
+        done(null, { user, token });
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+);
+
+export default passport;
