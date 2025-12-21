4e28e5343ae8f4f12a759a38639595762ccd1ac7
Rofilio <rofiliofernandes6@gmail.com>
2025-12-21 16:07:13 +0530

    Update google.js

--- backend/auth/google.js
+++ backend/auth/google.js
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
+import {User} from "../models/user.model.js";
 
 passport.use(
   new GoogleStrategy(
@@ -39,3 +39,4 @@ passport.use(
     }
   )
 );
+
